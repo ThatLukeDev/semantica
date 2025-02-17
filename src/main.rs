a145d3dc04f5f576788b9e3977981df7fe796995
Luke <76230394+ThatLukeDev@users.noreply.github.com>
2025-02-17 21:01:55 +0000

    similarity using dot product

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,32 @@
 use rust_bert::pipelines::sentence_embeddings;
 
+mod vector;
+
+use crate::vector::Dot;
+
 fn main() {
     let model = sentence_embeddings::SentenceEmbeddingsBuilder::remote(sentence_embeddings::SentenceEmbeddingsModelType::AllMiniLmL6V2).create_model()
         .unwrap();
 
-    let input = ["This is a sentence transformer example."];
+    let input = [
+        "Green cat",
+        "Red cat",
+        "Green dog",
+        "Red dog",
+    ];
 
     let embeddings: Vec<Vec<f32>> = model.encode(&input).unwrap();
+
+    for i in 0..input.len() {
+        for j in 0..input.len() {
+            println!(
+                "{} ({})\n{} ({})\n{}\n",
+                input[i],
+                embeddings[i].dot(&embeddings[i]).unwrap(),
+                input[j],
+                embeddings[j].dot(&embeddings[j]).unwrap(),
+                embeddings[i].dot(&embeddings[j]).unwrap()
+            );
+        }
+    }
 }
---
+++ src/vector.rs
@@ -0,0 +1,71 @@
+use std::ops::{Add, Mul};
+use std::error;
+use std::fmt;
+
+#[derive(Clone, Debug)]
+pub struct SizeMismatch;
+
+impl fmt::Display for SizeMismatch {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Input Vecs were not the same size.")
+    }
+}
+
+impl error::Error for SizeMismatch { }
+
+/// A trait for any type that can have a dot product calculated from itself and another of Self.
+pub trait Dot<T> {
+    /// Returns the dot product of self and other.
+    fn dot(&self, other: &Self) -> T;
+}
+
+/// An implementation of Dot for Vec
+///
+/// ```
+/// assert_eq!(
+///     vec![1, 2, 3].dot(vec![4, 5, 6])
+///         .unwrap(),
+///     32
+/// );
+/// ```
+impl<T: Copy + Add<Output = T> + Mul<Output = T> + From<i8>> Dot<Result<T, SizeMismatch>> for Vec<T> {
+    fn dot(&self, other: &Self) -> Result<T, SizeMismatch> {
+        let len = self.len();
+
+        if len != other.len() {
+            return Err(SizeMismatch);
+        }
+
+        let mut output = 0.into();
+
+        for i in 0..len {
+            output = output + self[i] * other[i];
+        }
+
+        Ok(output)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    pub fn dot() {
+        assert_eq!(
+            vec![1, 2, 3].dot(&vec![4, 5, 6])
+                .unwrap(),
+            32
+        );
+    }
+
+    #[test]
+    #[should_panic]
+    pub fn dot_panic() {
+        assert_eq!(
+            vec![1, 2, 3, 4].dot(&vec![4, 5, 6])
+                .unwrap(),
+            32
+        );
+    }
+}
