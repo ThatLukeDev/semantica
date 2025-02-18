ee674358ef458b3422e5116d1334741774406065
Luke <76230394+ThatLukeDev@users.noreply.github.com>
2025-02-18 18:05:45 +0000

    buckets

--- src/lib.rs
+++ src/lib.rs
@@ -2,23 +2,66 @@
 
 #![warn(missing_docs)]
 
-use rust_bert::pipelines::sentence_embeddings;
+use rust_bert::pipelines::sentence_embeddings::{self, SentenceEmbeddingsModel};
 
 pub mod vector;
 
 use crate::vector::Dot;
 
+const DIMENSION: usize = 384;
+const DIFFERENCE: f32 = 0.1;
+
 /// A 'hashmap' type object that semantically stores T by index of strings.
-pub struct SemanticVec<T> {
-    contents: Vec<(Vec<f32>, T)>
+pub struct SemanticVec<'a, T> {
+    contents: Vec<(Vec<f32>, T)>,
+
+    buckets: [Vec<&'a (Vec<f32>, T)>; DIMENSION],
+
+    model: SentenceEmbeddingsModel,
 }
 
-impl<T> SemanticVec<T> {
-    pub fn search(&self, s: &str) -> T {
-        let model = sentence_embeddings::SentenceEmbeddingsBuilder::remote(sentence_embeddings::SentenceEmbeddingsModelType::AllMiniLmL6V2).create_model()
-            .unwrap();
+impl<'a, T> SemanticVec<'a, T> {
+    /// Creates a new SemanticVec
+    pub fn new() -> Self
+    where T: std::fmt::Debug {
+        SemanticVec::<'a, _> {
+            contents: vec![],
+            buckets: vec![vec![]; DIMENSION].try_into().unwrap(),
+            model: sentence_embeddings::SentenceEmbeddingsBuilder::remote(sentence_embeddings::SentenceEmbeddingsModelType::AllMiniLmL6V2).create_model().unwrap()
+        }
+    }
 
-        let embeddings = model.encode(&[s])
+    /// Adds value to SemanticVec, and recalculates buckets.
+    pub fn add(&'a mut self, id: &str, val: T) {
+        let embeddings = self.model.encode(&[id])
+            .unwrap()[0].clone();
+
+        let mut buckets = vec!{};
+        let mut largest = 0.0;
+
+        for i in 0..DIMENSION {
+            let difference = embeddings[i] - largest;
+
+            if difference > 0.0 {
+                largest = embeddings[i];
+
+                if difference > DIFFERENCE {
+                    buckets.clear();
+                }
+
+                buckets.push(i);
+            }
+        }
+
+        self.contents.push((embeddings, val));
+
+        for i in buckets {
+            self.buckets[i].push(self.contents.last().unwrap());
+        }
+    }
+
+    pub fn search(&self, s: &str) -> T {
+        let embeddings = self.model.encode(&[s])
             .unwrap();
 
         todo!();
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,4 @@
-use rust_bert::pipelines::sentence_embeddings;
-
 mod vector;
-
-use crate::vector::Dot;
 
 fn main() {
 }
