2551b0e30596b60cdd40d7581de17ffaa060123d
Luke <76230394+ThatLukeDev@users.noreply.github.com>
2025-02-18 12:11:46 +0000

    start of semantic search

--- src/lib.rs
+++ src/lib.rs
@@ -2,4 +2,25 @@
 
 #![warn(missing_docs)]
 
+use rust_bert::pipelines::sentence_embeddings;
+
 pub mod vector;
+
+use crate::vector::Dot;
+
+/// A 'hashmap' type object that semantically stores T by index of strings.
+pub struct SemanticVec<T> {
+    contents: Vec<(Vec<f32>, T)>
+}
+
+impl<T> SemanticVec<T> {
+    pub fn search(&self, s: &str) -> T {
+        let model = sentence_embeddings::SentenceEmbeddingsBuilder::remote(sentence_embeddings::SentenceEmbeddingsModelType::AllMiniLmL6V2).create_model()
+            .unwrap();
+
+        let embeddings = model.encode(&[s])
+            .unwrap();
+
+        todo!();
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -5,28 +5,4 @@ mod vector;
 use crate::vector::Dot;
 
 fn main() {
-    let model = sentence_embeddings::SentenceEmbeddingsBuilder::remote(sentence_embeddings::SentenceEmbeddingsModelType::AllMiniLmL6V2).create_model()
-        .unwrap();
-
-    let input = [
-        "Green cat with a spiral tail",
-        "Red cat with a straight tail",
-        "Green dog with a straight tail",
-        "Red dog with a spiral tail",
-    ];
-
-    let embeddings: Vec<Vec<f32>> = model.encode(&input).unwrap();
-
-    for i in 0..input.len() {
-        for j in 0..input.len() {
-            println!(
-                "{} ({})\n{} ({})\n{}\n",
-                input[i],
-                embeddings[i].dot(&embeddings[i]).unwrap(),
-                input[j],
-                embeddings[j].dot(&embeddings[j]).unwrap(),
-                embeddings[i].dot(&embeddings[j]).unwrap()
-            );
-        }
-    }
 }
