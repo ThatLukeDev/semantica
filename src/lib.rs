0407f83fba11efd0ea871a741a1abfeee64ddb4a
ThatLukeDev <luke@l2j.co.uk>
2025-02-25 08:39:02 +0000

    O(logn) (possibly) model idea

--- src/lib.rs
+++ src/lib.rs
@@ -10,93 +10,84 @@ pub mod vector;
 use crate::vector::Dot;
 
 const DIMENSION: usize = 384;
-const DIFFERENCE: f32 = 0.1;
 
-/// A 'hashmap' type object that semantically stores T by index of strings.
-pub struct SemanticVec<'a, T> {
-    contents: Vec<(Vec<f32>, T)>,
-
-    buckets: [Vec<&'a (Vec<f32>, T)>; DIMENSION],
+/// An object that semantically stores T by index of strings.
+pub struct SemanticVec<T> {
+    contents: Vec<(Vec<f32>, T, f32)>,
 
     model: SentenceEmbeddingsModel,
+
+    /// A linear dot transformer.
+    /// Typically should be in the form
+    /// `vec!![0..DIMENSION]`
+    ldt: Vec<f32>,
 }
 
-impl<'a, T> SemanticVec<'a, T> {
+impl<T: Clone> SemanticVec<T> {
     /// Creates a new SemanticVec
     pub fn new() -> Self
     where T: std::fmt::Debug {
-        SemanticVec::<'a, _> {
+        SemanticVec::<_> {
             contents: vec![],
-            buckets: vec![vec![]; DIMENSION].try_into().unwrap(),
-            model: sentence_embeddings::SentenceEmbeddingsBuilder::remote(sentence_embeddings::SentenceEmbeddingsModelType::AllMiniLmL6V2).create_model().unwrap()
+            model: sentence_embeddings::SentenceEmbeddingsBuilder::remote(sentence_embeddings::SentenceEmbeddingsModelType::AllMiniLmL6V2).create_model().unwrap(),
+            ldt: (0..DIMENSION).map(|v| v as f32).collect()
         }
     }
 
-    /// Adds value to SemanticVec, and recalculates buckets.
-    pub fn add(&'a mut self, id: &str, val: T) {
-        let embeddings = self.model.encode(&[id])
-            .unwrap()[0].clone();
+    fn quick_search(&self, ldt: f32) -> usize {
+        let mut start = 0;
+        let mut end = self.contents.len();
+        let mut mid = (start + end) / 2;
 
-        let mut buckets = vec!{};
-        let mut largest = 0.0;
-
-        for i in 0..DIMENSION {
-            let difference = embeddings[i] - largest;
-
-            if difference > 0.0 {
-                largest = embeddings[i];
-
-               if difference > DIFFERENCE {
-                    buckets.clear();
-                }
-
-                buckets.push(i);
+        while start < end {
+            if ldt > self.contents[mid].2 {
+                start = mid + 1;
             }
+            else {
+                end = mid;
+            }
+
+            mid = (start + end) / 2;
         }
 
-        self.contents.push((embeddings, val));
+        mid
+    }
 
-        for i in buckets {
-            self.buckets[i].push(self.contents.last().unwrap());
-        }
+    /// Adds value to SemanticVec, and recalculates linear dot product.
+    pub fn add(&mut self, id: &str, val: T) {
+        todo!();
+
+        let embeddings = self.model.encode(&[id])
+            .unwrap().concat();
+
+        let ldt = embeddings.dot(&self.ldt).unwrap();
+
+        let entry = (embeddings, val, ldt);
+
+        let predicted = self.quick_search(ldt);
     }
 
     /// Finds the closest match to input string.
     pub fn search(&self, s: &str) -> Option<&T> {
-        let embeddings = self.model.encode(&[s])
-            .unwrap();
-
-        let mut buckets = vec!{};
-        let mut largest = 0.0;
-
-        for i in 0..DIMENSION {
-            let difference = embeddings[0][i] - largest;
-
-            if difference > 0.0 {
-                largest = embeddings[0][i];
-
-                if difference > DIFFERENCE {
-                    buckets.clear();
-                }
-
-                buckets.push(i);
-            }
-        }
-
-        let mut closest_match = None;
-        let mut closest_cos_dot = 0.0;
-
-        for i in buckets {
-            for item in &self.buckets[i] {
-                let cos_dot = item.0.dot(&embeddings[0]).unwrap();
-
-                if cos_dot > closest_cos_dot {
-                    closest_cos_dot = cos_dot;
-                    closest_match = Some(&item.1);
-                }
-            }
-        }
-
-        closest_match
+        todo!();
     }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn quick_search_internal() {
+        let db = SemanticVec::<i32> {
+            contents: vec![(vec![], 0, 1.2), (vec![], 0, 2.0), (vec![], 0, 3.6), (vec![], 0, 12.2), (vec![], 0, 19.3)],
+            model: sentence_embeddings::SentenceEmbeddingsBuilder::remote(sentence_embeddings::SentenceEmbeddingsModelType::AllMiniLmL6V2).create_model().unwrap(),
+            ldt: (0..DIMENSION).map(|v| v as f32).collect()
+        };
+
+        assert_eq!(db.quick_search(12.2), 3);
+        assert_eq!(db.quick_search(3.6), 2);
+    }
+
+    // TODO: impl tests
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,19 @@
-mod vector;
+use semantica::SemanticVec;
 
 fn main() {
+    let mut db: SemanticVec<i32> = SemanticVec::new();
+
+    db.add("One", 1);
+    db.add("Two", 2);
+    db.add("Three", 3);
+    db.add("Four", 4);
+    db.add("Five", 5);
+
+    db.add("Dog", 0);
+    db.add("Cat", -1);
+
+    match db.search("uno") {
+        Some(item) => println!("{}", item),
+        None => ()
+    }
 }
