823be6f09c54a94cdb45a0fae195416ab103f31d
ThatLukeDev <luke@l2j.co.uk>
2025-02-25 08:51:45 +0000

    add function moved on vec of LDTs, which is far faster, moving 12 bytes per val, not on the order of thousands

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,9 @@ const DIMENSION: usize = 384;
 
 /// An object that semantically stores T by index of strings.
 pub struct SemanticVec<T> {
-    contents: Vec<(Vec<f32>, T, f32)>,
+    contents: Vec<(Vec<f32>, T)>,
+
+    ldts: Vec<(usize, f32)>,
 
     model: SentenceEmbeddingsModel,
 
@@ -29,6 +31,7 @@ impl<T: Clone> SemanticVec<T> {
     where T: std::fmt::Debug {
         SemanticVec::<_> {
             contents: vec![],
+            ldts: vec![],
             model: sentence_embeddings::SentenceEmbeddingsBuilder::remote(sentence_embeddings::SentenceEmbeddingsModelType::AllMiniLmL6V2).create_model().unwrap(),
             ldt: (0..DIMENSION).map(|v| v as f32).collect()
         }
@@ -36,11 +39,11 @@ impl<T: Clone> SemanticVec<T> {
 
     fn quick_search(&self, ldt: f32) -> usize {
         let mut start = 0;
-        let mut end = self.contents.len();
+        let mut end = self.ldts.len();
         let mut mid = (start + end) / 2;
 
         while start < end {
-            if ldt > self.contents[mid].2 {
+            if ldt > self.ldts[mid].1 {
                 start = mid + 1;
             }
             else {
@@ -55,20 +58,27 @@ impl<T: Clone> SemanticVec<T> {
 
     /// Adds value to SemanticVec, and recalculates linear dot product.
     pub fn add(&mut self, id: &str, val: T) {
-        todo!();
-
         let embeddings = self.model.encode(&[id])
             .unwrap().concat();
 
         let ldt = embeddings.dot(&self.ldt).unwrap();
 
-        let entry = (embeddings, val, ldt);
-
         let predicted = self.quick_search(ldt);
+
+        self.contents.push((embeddings, val));
+
+        self.ldts.insert(predicted, (self.contents.len() - 1, ldt));
     }
 
     /// Finds the closest match to input string.
     pub fn search(&self, s: &str) -> Option<&T> {
+        let embeddings = self.model.encode(&[s])
+            .unwrap().concat();
+
+        let ldt = embeddings.dot(&self.ldt).unwrap();
+
+        let predicted = self.quick_search(ldt);
+
         todo!();
     }
 }
@@ -80,7 +90,8 @@ mod tests {
     #[test]
     fn quick_search_internal() {
         let db = SemanticVec::<i32> {
-            contents: vec![(vec![], 0, 1.2), (vec![], 0, 2.0), (vec![], 0, 3.6), (vec![], 0, 12.2), (vec![], 0, 19.3)],
+            contents: vec![],
+            ldts: vec![(0, 1.2), (0, 2.0), (0, 3.6), (0, 12.2), (0, 19.3)],
             model: sentence_embeddings::SentenceEmbeddingsBuilder::remote(sentence_embeddings::SentenceEmbeddingsModelType::AllMiniLmL6V2).create_model().unwrap(),
             ldt: (0..DIMENSION).map(|v| v as f32).collect()
         };
