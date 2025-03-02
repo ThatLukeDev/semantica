fa8001467ba3ef98cbb639da285ef2689ac201c9
ThatLukeDev <luke@l2j.co.uk>
2025-03-02 14:38:41 +0000

    byte conversion for string

--- src/byte_conversion.rs
+++ src/byte_conversion.rs
@@ -11,7 +11,7 @@ macro_rules! default_impl_ByteConversion {
     ($($t:ty),+) => {
         $(impl ByteConversion for $t {
             fn to_bytes(self) -> Vec<u8> {
-                self.to_be_bytes().into_iter().collect()
+                self.to_be_bytes().to_vec()
             }
 
             fn from_bytes(input: Vec<u8>) -> Self {
@@ -22,6 +22,16 @@ macro_rules! default_impl_ByteConversion {
 }
 
 default_impl_ByteConversion!(usize, u8, u16, u32, u64, u128, i8, i16, i32, i64, i128, f32, f64);
+
+impl ByteConversion for String {
+    fn to_bytes(self) -> Vec<u8> {
+        self.as_bytes().to_vec()
+    }
+
+    fn from_bytes(input: Vec<u8>) -> Self {
+        String::from_utf8(input).unwrap()
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -39,5 +49,14 @@ mod tests {
     #[test]
     fn num_types() {
         test_default_impl_ByteConversion!(usize, u8, u16, u32, u64, u128, i8, i16, i32, i64, i128, f32, f64);
+    }
+
+    #[test]
+    fn string_types() {
+        let str = "Hello World!";
+        assert_eq!(
+            String::from_bytes(str.to_string().to_bytes()),
+            str.to_string()
+        );
     }
 }
