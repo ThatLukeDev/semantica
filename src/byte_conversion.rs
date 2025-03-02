aaca70bbf6e50c124048a0048e4b3f244116a72f
ThatLukeDev <luke@l2j.co.uk>
2025-03-02 14:27:07 +0000

    byte conversions for all num types

--- src/byte_conversion.rs
+++ src/byte_conversion.rs
@@ -6,3 +6,38 @@ pub trait ByteConversion {
     /// Converts Vec<u8> to Self.
     fn from_bytes(input: Vec<u8>) -> Self;
 }
+
+macro_rules! default_impl_ByteConversion {
+    ($($t:ty),+) => {
+        $(impl ByteConversion for $t {
+            fn to_bytes(self) -> Vec<u8> {
+                self.to_be_bytes().into_iter().collect()
+            }
+
+            fn from_bytes(input: Vec<u8>) -> Self {
+                Self::from_be_bytes(input.try_into().unwrap())
+            }
+        })+
+    }
+}
+
+default_impl_ByteConversion!(usize, u8, u16, u32, u64, u128, i8, i16, i32, i64, i128, f32, f64);
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    macro_rules! test_default_impl_ByteConversion {
+        ($($t:tt),+) => {
+            $(let val: $t = $t::MAX; assert_eq!(
+                    $t::from_bytes(val.to_bytes()),
+                    val
+            );)+
+        }
+    }
+
+    #[test]
+    fn num_types() {
+        test_default_impl_ByteConversion!(usize, u8, u16, u32, u64, u128, i8, i16, i32, i64, i128, f32, f64);
+    }
+}
